6cdffe533784f4ee72aafd46113faaa897a86ca5
aashimamehta <58320925+aashimamehta@users.noreply.github.com>
2021-04-18 18:45:33 -0700

    Added Socket-Communication files

---
+++ serverMessage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import socket
+
+s = socket.socket() #socket.AF_INET, socket.SOCK_STREAM)
+port = 5001 #same as server
+host = '0.0.0.0' #0.0.0.0 catches any client tryna connect. 
+#not sure why the actual IP of client wasn't working
+s.bind((host, port))
+s.listen(5)
+
+while True:
+    # now our endpoint knows about the OTHER endpoint.
+    clientsocket, address = s.accept()
+    print("Connection from {} has been established.".format(address))
+    clientsocket.send(bytes("Hey there!!!","utf-8"))
+    clientsocket.close()
